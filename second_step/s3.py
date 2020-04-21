d46e97b519297ec96140458e516dfbde6dca97f5
xiao.xinmiao <xiaoxm_001@163.com>
2020-04-22 06:51:50 +0800

    regex 2

--- second_step/s3.py
+++ second_step/s3.py
@@ -11,3 +11,17 @@ print(fetch("/v1/fruits/1"))
 print(fetch("/v1/fruits"))
 
 
+print("===="*20)
+
+
+def fetch2(contents):
+    pat = re.compile(r'(^|\?|\&)name='+r'(.*?)(&|$)')
+    pMatch = re.search(pat,contents)
+    return pMatch.group(2)
+
+
+print(fetch2("name=xxm"))
+print(fetch2("/v1/fruits?name=lm"))
+print(fetch2("/v1/fruits?name=ldeh&age=2"))
+print(fetch2("/v1/fruits?sex=1&name=zyx"))
+print(fetch2("/v1/fruits?sex=1&name=gfc&age=2"))
